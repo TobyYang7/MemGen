9b7db3a5e3827113a2ccc68bb0af7ec7d1342b24
TobyYang7 <tobyyang7@outlook.com>
2025-10-22 20:00:10 +0000

    add mm_math env

--- larm/data/envs/__init__.py
+++ larm/data/envs/__init__.py
@@ -3,3 +3,4 @@ from larm.data.envs.gsm8k_env import *
 from larm.data.envs.gpqa_env import *
 from larm.data.envs.kodcode_env import *
 from larm.data.envs.triviaqa_env import *
+from larm.data.envs.mm_math_env import *
